b080bcff597b24e4042e7d1aa0e9d6c61c172d33
Onkar Vhatkar <onkar.vhatkar23@gmail.com>
2020-07-18 15:34:35 +0530

    virus icons added

--- src/features/countryDetails/components/countryName/index.js
+++ src/features/countryDetails/components/countryName/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import { FontSizes, Color } from '../../../../constants'
 
 const CountryName = (props) => {
   const { containerStyle, name } = props
   return (
     <View style={containerStyle}>
+      <Image style={styles.virusIconStart} source={require('../../../../assets/virus-96.png')} />
       <Text style={styles.country}>{name}</Text>
+      <Image style={styles.virusIconEnd} source={require('../../../../assets/virus-64.png')} />
     </View>
   )
 }
@@ -15,6 +17,16 @@ const styles = StyleSheet.create({
   country: {
     fontSize: FontSizes.MEDIUM,
     color: Color.TEXT_BLACK,
+  },
+  virusIconStart: {
+    position: 'absolute',
+    start: 40,
+    top: 15,
+  },
+  virusIconEnd: {
+    position: 'absolute',
+    end: 10,
+    bottom: 10,
   },
 })
 
